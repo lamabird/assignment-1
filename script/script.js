22349fe7a14b4e0a8336899fa013391567568878
liam <liamwilsonliamwilson@gmail.com>
2023-04-07 13:48:39 -0700

    added a start of darktheme code

---
+++ script/script.js
@@ -0,0 +1,34 @@
+const themeToggle = document.getElementById('theme-toggle');
+themeToggle.addEventListener('click', () => {
+    const body = document.body;
+    body.classList.toggle('dark-theme');
+    const divA = document.querySelector('.div_a');
+    divA.classList.toggle('dark-theme');
+    const divB = document.querySelector('.div_b');
+    divB.classList.toggle('dark-theme');
+    const header = document.querySelector('.header');
+    header.classList.toggle('dark-theme');
+    const header2 = document.querySelector('.header2');
+    header2.classList.toggle('dark-theme');
+    const toggleButtons = document.querySelectorAll('.toggle');
+    toggleButtons.forEach(button => button.classList.toggle('dark-theme'));
+    const div0 = document.querySelector('.div_0');
+    div0.classList.toggle('dark-theme');
+    const div1 = document.querySelector('.div_1');
+    div1.classList.toggle('dark-theme');
+    const div2 = document.querySelector('.div_2');
+    div2.classList.toggle('dark-theme');
+    const div3 = document.querySelector('.div_3');
+    div3.classList.toggle('dark-theme');
+    const div4 = document.querySelector('.div_4');
+    div4.classList.toggle('dark-theme');
+    const img1 = document.querySelector('.img1');
+    img1.classList.toggle('dark-theme');
+    const img2 = document.querySelector('.img2');
+    img2.classList.toggle('dark-theme');
+    if (body.classList.contains('dark-theme')) {
+        themeToggle.textContent = 'Light Theme';
+    } else {
+        themeToggle.textContent = 'Dark Theme';
+    }
+});
